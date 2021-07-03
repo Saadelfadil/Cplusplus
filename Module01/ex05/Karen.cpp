5b75134a624286bbbe7fedf5cb75f376f4d0bad3
Saad El fadil <sel-fadi@e0r5p2.1337.ma>
2021-07-03 22:02:42 +0100

    start ex05

--- Module01/ex05/Karen.cpp
+++ Module01/ex05/Karen.cpp
@@ -6,8 +6,43 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 20:34:22 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/07/03 20:38:11 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/07/03 21:14:35 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
+
+Karen::Karen()
+{
+    
+}
+
+Karen::~Karen()
+{
+    
+}
+
+void complain(std::string level)
+{
+
+}
+
+void Karen::debug(void)
+{
+    std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << "\n";
+}
+
+void Karen::info(void)
+{
+    std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!" << "\n";
+}
+
+void Karen::warning(void)
+{
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << "\n";
+}
+
+void Karen::error(void)
+{
+    std::cout << "This is unacceptable, I want to speak to the manager now." << "\n";
+}
--- Module01/ex05/Karen.hpp
+++ Module01/ex05/Karen.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/03 20:34:25 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/07/03 20:38:26 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/07/03 21:09:53 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@
 
 class Karen {
     private :
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
     public :
-        
+        ~Karen();
+        Karen();
+        void complain(std::string level);
 };
 
 #endif
