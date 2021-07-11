3158687312c4eb8f7457e57b741dcf71cb909196
Saadelfadil <saadelfadil18@gmail.com>
2021-07-11 19:19:21 +0100

    CPP_04

---
+++ Module04/ex01/AWeapon.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AWeapon.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/11 13:13:46 by sel-fadi          #+#    #+#             */
+/*   Updated: 2021/07/11 18:40:25 by sel-fadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SORCERER_HPP
+# define SORCERER_HPP
+
+# include <iostream>
+# include <string>
+
+class AWeapon
+{
+	protected:
+		std::string _name;
+		int			_apcost;
+		int			_damage;
+	public:
+		AWeapon(std::string const & name, int apcost, int damage);
+		~AWeapon();
+		AWeapon();
+		AWeapon::AWeapon(const AWeapon & src);
+		std::string getName() const;
+		int getAPCost() const;
+		int getDamage() const;
+		void attack() const = 0;
+};
+
+
+#endif /* ******************************************************** SORCERER_H */
---
+++ Module04/ex01/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/11 13:36:40 by sel-fadi          #+#    #+#             */
+/*   Updated: 2021/07/11 18:22:53 by sel-fadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AWeapon.hpp"
+
+int main()
+{
+    Sorcerer robert("Robert", "the Magnificent");
+    Victim jim("Jimmy");
+    Peon joe("Joe");
+    std::cout << robert << jim << joe;
+    robert.polymorph(jim);
+    robert.polymorph(joe);
+    return 0;
+}
