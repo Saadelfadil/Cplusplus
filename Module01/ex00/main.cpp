9d731d479e8d52e3a28f0c51eb03aa0ddbb65b50
saadelfadil <saadelfadil18@gmail.com>
2021-09-29 08:57:47 +0100

    CPP_01

--- Module01/ex00/main.cpp
+++ Module01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 12:08:34 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/07/03 11:34:53 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/09/29 08:57:29 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(void)
 {
-	Zombie *zombii = newZombie("hicham");
+	Zombie *zombii = newZombie("hfadyl");
 	zombii->announce();
 	delete zombii;
 	randomChump("sel-fadi");
