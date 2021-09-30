d08ee4b97251a72f49fbafa14f3bbf35c4eea039
saadelfadil <saadelfadil18@gmail.com>
2021-09-30 19:37:05 +0100

    CPP_08

---
+++ Module08/ex01/span.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/04 12:08:16 by sel-fadi          #+#    #+#             */
+/*   Updated: 2021/09/30 19:36:51 by sel-fadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_H
+# define SPAN_H
+
+# include <iostream>
+# include <string>
+#include <cctype>
+# include <algorithm>
+
+template <class N>
+class Span
+{
+    private:
+        unsigned int _n; 
+    public:
+        Span(unsigned int n);
+};
+
+#endif
