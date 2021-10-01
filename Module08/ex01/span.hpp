440e504ac692d7271438f5ced93234278796b88b
saadelfadil <saadelfadil18@gmail.com>
2021-10-01 11:41:19 +0100

    CPP_08

--- Module08/ex01/span.hpp
+++ Module08/ex01/span.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/04 12:08:16 by sel-fadi          #+#    #+#             */
-/*   Updated: 2021/09/30 19:36:51 by sel-fadi         ###   ########.fr       */
+/*   Updated: 2021/10/01 11:38:12 by sel-fadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,19 @@
 #include <cctype>
 # include <algorithm>
 
-template <class N>
 class Span
 {
     private:
-        unsigned int _n; 
+        unsigned int _N;
     public:
         Span(unsigned int n);
+        void addNumber(unsigned int n);
+        
+        class alreadyStored : public std::exception
+		{
+			public:
+				const char *what() const throw();
+		};
 };
 
 #endif
