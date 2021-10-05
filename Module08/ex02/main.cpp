57e57586d31265f10d8be747c4de6c86821ebb60
saadelfadil <saadelfadil18@gmail.com>
2021-10-05 20:04:23 +0100

    CPP_08

---
+++ Module08/ex02/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/05 12:55:38 by sel-fadi          #+#    #+#             */
+/*   Updated: 2021/10/05 19:39:02 by sel-fadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+#include <array>
+
+int main( void )
+{
+	
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
---
+++ Module08/ex02/mutantstack.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-fadi <sel-fadi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/04 12:08:16 by sel-fadi          #+#    #+#             */
+/*   Updated: 2021/10/05 20:03:46 by sel-fadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef mutantstack_H
+# define mutantstack_H
+
+# include <iostream>
+# include <string>
+# include  <stack>
+# include <iterator>
+
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack(){}
+        ~MutantStack(){}
+        MutantStack(MutantStack<T> const &obj)
+        {
+            (void)obj;
+            *this = obj;
+        }
+        MutantStack & operator = (MutantStack<T> const &obj)
+        {
+            (void)obj;
+            return *this;
+        }
+		typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin()
+        {
+            return  this->c.begin();
+        }
+        iterator end()
+        {
+            return  this->c.end();
+        }
+};
+
+#endif
